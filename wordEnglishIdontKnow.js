ea181d08efff94b1c7db2749a98a8c2b1c4ba18c
= <=>
2024-09-18 21:41:24 -0300

    first commit

---
+++ wordEnglishIdontKnow.js
@@ -0,0 +1,17 @@
+const unknowWords = {
+    willing:"dispuesto",
+    invest:"invertir",
+    carving:"tallado", //--> Carve
+    "How to cover":"",
+    "homeostasis":"",
+    "take shape":"",
+    "act on":"",
+    likely:"",
+    slightly:"",
+    fulfilling:"",
+    tend:"",
+    "point out":"",
+    held:""
+
+}
+"likely slightly fulfilling tend point out held"
