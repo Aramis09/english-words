f7dd5f3857786ff2297cc1e5f89156a80d113fb4
Aramis jaime <you@example.com>
2024-12-20 01:55:20 -0300

    new words

--- wordEnglishIdontKnow.js
+++ wordEnglishIdontKnow.js
@@ -8,10 +8,30 @@ const unknowWords = {
     "act on":"",
     likely:"",
     slightly:"",
-    fulfilling:"",
+    fulfill:"hacer realidad algo",
     tend:"",
-    "point out":"",
-    held:""
+    "point out":"senialar o hacer notar",
+    held:"sostuvo",
+    sassy:"Atrevido",
+    blew:"explotar",
+    nail:"clavo o conseguir",
+    witty:"ingenioso"
 
 }
 "likely slightly fulfilling tend point out held"
+
+const program = {
+    "run, launch, boot":"To start a program on a computer",
+    abort:"it is  when a program crahsed and the user close it ande lost the unsaved changes",
+    terminate:"To cause a process or task to stop executing",
+    feature:"it is  something that a computer program is supposed to do",
+    bug:"it is a poorly made feature",
+    error:"it is something that abort or crashed the program",
+    "end user":"the people who will use the program as a service"
+}
+
+const emailing = {
+    coverLetter:"The letter that you write when you are applying for a job",
+    acceptanceLetter:"When you are welcome to the company",
+    rejectedLetter:"When you are not accepted for the job"
+}
